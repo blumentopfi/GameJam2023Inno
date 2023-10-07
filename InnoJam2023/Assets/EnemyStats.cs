8cbe36befa90d1955b4b348e2541b750c5a920a3
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2023-10-07 10:11:19 +0200

    Extract stats to own class

---
+++ InnoJam2023/Assets/EnemyStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float baseSpeed { get; set; }
+    public float BaseHealth { get; set; }
+    public float baseDamage { get; set; }
+
+}
