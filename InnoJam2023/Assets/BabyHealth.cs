dde87c72415e62ebe0abf59a4ec58589bb1dd16e
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2023-10-08 11:04:54 +0200

    now code is valid again

--- InnoJam2023/Assets/BabyHealth.cs
+++ InnoJam2023/Assets/BabyHealth.cs
@@ -18,7 +18,7 @@ public class BabyHealth : MonoBehaviour
         traumaDisplay.text = $"Trauma:{health}%";
     }
 
-    public void TakeDamage( float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
 
@@ -26,6 +26,7 @@ public class BabyHealth : MonoBehaviour
         {
             SceneManager.LoadScene(2);
         }
+    }
 
     public int GetBabyHealth()
     {
