ef575cc099e28940b1c3cb939a4028d61fcbe671
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2023-10-08 12:04:11 +0200

    Now also correctly sees hp bars

---
+++ InnoJam2023/Assets/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pauseGame();
+        }
+    }
+
+    public void startGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void pauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void quitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
