d3ca1064af21105f76e0927ac7ce665ad9f7419b
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2023-10-08 11:00:22 +0200

    start and end screen

--- InnoJam2023/Assets/BabyHealth.cs
+++ InnoJam2023/Assets/BabyHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BabyHealth : MonoBehaviour
 {
@@ -26,7 +27,7 @@ public class BabyHealth : MonoBehaviour
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(2);
         }
 
     }
